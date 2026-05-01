ca765dd81c2a84a61e5bcfaf90da080c2eefa1df
Raul691 <cuisinha.612@gmail.com>
2026-05-01 13:41:19 -0300

    feat: adição de prioridade nas tarefas (mudança de escopo)

--- src/app.py
+++ src/app.py
@@ -8,34 +8,49 @@ tarefas = []
 def home():
     return render_template('index.html', tarefas=tarefas)
 
+
 @app.route('/tarefas_web', methods=['POST'])
 def criar_tarefa_web():
     nome = request.form.get('nome')
     status = request.form.get('status')
+    prioridade = request.form.get('prioridade')
 
     tarefas.append({
         "nome": nome,
-        "status": status
+        "status": status,
+        "prioridade": prioridade if prioridade else "normal"
     })
 
     return redirect('/')
+
 
 @app.route('/tarefas', methods=['GET'])
 def listar_tarefas():
     return jsonify(tarefas)
 
+
 @app.route('/tarefas', methods=['POST'])
 def criar_tarefa():
-    nova_tarefa = request.json
+    nova_tarefa = {
+        "nome": request.json.get("nome"),
+        "status": request.json.get("status"),
+        "prioridade": request.json.get("prioridade", "normal")
+    }
     tarefas.append(nova_tarefa)
     return jsonify(nova_tarefa), 201
+
 
 @app.route('/tarefas/<int:id>', methods=['PUT'])
 def atualizar_tarefa(id):
     if id < len(tarefas):
-        tarefas[id] = request.json
+        tarefas[id] = {
+            "nome": request.json.get("nome"),
+            "status": request.json.get("status"),
+            "prioridade": request.json.get("prioridade", "normal")
+        }
         return jsonify(tarefas[id])
     return "Tarefa não encontrada", 404
+
 
 @app.route('/tarefas/<int:id>', methods=['DELETE'])
 def deletar_tarefa(id):
@@ -43,6 +58,7 @@ def deletar_tarefa(id):
         tarefa_removida = tarefas.pop(id)
         return jsonify(tarefa_removida)
     return "Tarefa não encontrada", 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
